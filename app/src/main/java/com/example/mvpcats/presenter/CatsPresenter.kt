acf0c0b8a405724829774c0c8be8804faa3f5873
Supreme1Duck <Supreme1Duck2020>
2021-07-14 00:16:55 +0300

    Saving Images 2

--- app/src/main/java/com/example/mvpcats/presenter/CatsPresenter.kt
+++ app/src/main/java/com/example/mvpcats/presenter/CatsPresenter.kt
@@ -1,14 +1,11 @@
 package com.example.mvpcats.presenter
 
-import android.app.Activity
 import android.app.Application
-import android.content.ContentValues
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
-import android.os.Environment
-import android.provider.MediaStore
+import android.os.Build
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
+import androidx.annotation.RequiresApi
 import com.example.mvpcats.model.database.Cats
 import com.example.mvpcats.model.entity.CatsModel
 import com.example.mvpcats.model.repository.CatsRepository
@@ -19,12 +16,10 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import java.io.File
 import java.io.FileOutputStream
-import java.util.*
-import kotlin.collections.HashSet
 
 @Suppress("UNCHECKED_CAST")
 class CatsPresenter<T>(
-    activity: MainContract.MarkerView<T>,
+    private var activity: MainContract.MarkerView<T>,
     var application: Application
 ) : MainContract.Presenter {
 
@@ -88,23 +83,12 @@ class CatsPresenter<T>(
         )
     }
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun downloadImage(bitmapDrawable: BitmapDrawable) {
-
-//        val contentResolver = application.contentResolver
-//        val contentValues = ContentValues()
-//        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "Image_" + ".jpg")
-//        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
-//        contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
-//        val imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-//
-//        val outputStream = contentResolver.openOutputStream(imageUri!!)
-//        bitmapDrawable.bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-//        ActivityCompat.requestPermissions(catsView as Activity, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
-
         val bitmap = bitmapDrawable.bitmap
 
         val filePath = application.getExternalFilesDir(null)
-        val dir = File(filePath!!.absolutePath)
+        val dir = File(filePath!!.absolutePath + "/downloads")
         dir.mkdir()
         val file = File(dir, System.currentTimeMillis().toString() + ".jpg")
 
